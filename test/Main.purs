eddf51971e592cb8c89b2e53151790dbd0626aa3
Stefan Holzmüller <stefanholzmueller@github>
2016-01-23 22:30:23 +0100

    quickcheck

--- test/Main.purs
+++ test/Main.purs
@@ -1,9 +1,12 @@
 module Test.Main where
 
 import Prelude
-import Control.Monad.Eff
-import Control.Monad.Eff.Exception
-import Test.Yahtzee
+import Test.QuickCheck
 
-main :: forall e. Eff (err :: EXCEPTION | e) Unit
-main = tests
+
+main :: forall eff. QC eff Unit
+main = do
+	pips2 <- choose (1, 6)
+        pips3 <- choose (1, 6)
+	let dice5 = [pips2, pips2, pips3, pips3, pips3]
+	quickCheck \n -> n + 1 > n
