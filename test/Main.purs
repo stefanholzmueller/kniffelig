a94bfb3c0c839c6393c290092208c081837953a2
Stefan Holzmüller <stefanholzmueller@github>
2016-02-18 16:51:12 +0100

    more testing with QuickCheck

--- test/Main.purs
+++ test/Main.purs
@@ -1,33 +1,50 @@
 module Test.Main where
 
-import Prelude (Unit, bind)
-import Control.Monad.Eff (Eff)
+import Prelude (Eq, Show, Unit, bind, pure, return)
+import Control.Monad.Eff (Eff, runPure)
 import Control.Monad.Eff.Console (CONSOLE, print)
+import Control.Monad.Eff.Exception
 import Control.Monad.Eff.Random (RANDOM)
---import Data.Array
-import Durstenfeld (shuffle)
---import Test.QuickCheck
---import Test.QuickCheck.Gen
-
-type RNG x = forall eff. Eff (random :: RANDOM | eff) x
-
-dice :: RNG (Array Int)
-dice = shuffle [1,2,3,4,5]
-
---main :: forall eff. Eff (random :: RANDOM, console :: CONSOLE | eff) Unit
-main :: Eff (random :: RANDOM, console :: CONSOLE) Unit
-main = do
-	d <- dice
- 	print d
+import Data.Maybe
+--import Durstenfeld (shuffle)
+import Yahtzee
+import Test.QuickCheck
+import Test.QuickCheck.Arbitrary
+import Test.QuickCheck.Gen (Gen, elements, chooseInt)
 
 
---shuffled :: forall eff. Eff (random :: RANDOM | eff) (Array Int)
---shuffled = Durstenfeld.shuffle [1,2,3,4]
+data Ternary = Yes | No | Unknown
 
---main :: forall eff. QC eff Unit
---main = do
---        pips2 <- chooseInt 1 6
---        pips3 <- chooseInt 1 6
---        let dice5 = [pips2, pips2, pips3, pips3, pips3]
---        let dice = shuffle dice5
---        quickCheck \n -> n + 1 > n
+instance eqTernary :: Eq Ternary where
+  eq Yes Yes = true
+  eq No No = true
+  eq Unknown Unknown = true
+  eq _ _ = false
+
+instance showTernary :: Show Ternary where
+  show Yes = "Y"
+  show No = "N"
+  show Unknown = "U"
+
+instance arbTernary :: Arbitrary Ternary where
+  arbitrary = elements Yes [Yes, No, Unknown]
+
+prop_true :: Ternary -> Result
+prop_true t = t === t
+
+
+data Dice = Dice (Array Int)
+
+instance arbitraryDice :: Arbitrary Dice where
+  arbitrary = genFullHouse
+
+main :: forall eff. QC eff Unit
+main = quickCheck (propFullHouse :: Dice -> Result)
+
+genFullHouse :: Gen Dice
+genFullHouse = do pips2 <- chooseInt 1 6
+                  pips3 <- chooseInt 1 6
+                  return (Dice [pips2, pips2, pips3, pips3, pips3])
+
+propFullHouse (Dice dice) = runPure (catchException handler (scoreStr "FullHouse" dice)) === Just 25
+  where handler error = pure Nothing
