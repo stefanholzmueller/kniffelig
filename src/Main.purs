139ff429db7ade8600002f35bb9f68bd2e800258
Stefan Holzmüller <stefanholzmueller@github>
2016-03-17 07:07:16 +0100

    rolling dice

--- src/Main.purs
+++ src/Main.purs
@@ -6,7 +6,9 @@ import Control.Monad.Aff (Aff(), runAff)
 import Control.Monad.Eff (Eff())
 import Control.Monad.Eff.Exception (throwException)
 import Control.Monad.Eff.Random
+import Data.Array (replicate)
 import Data.Maybe
+import Data.Traversable (sequence)
 
 import Halogen
 import Halogen.Util (appendToBody, onLoad)
@@ -17,12 +19,14 @@ import Yahtzee
 
 
 data Query a = ScoreQuery Category a
+	     | Roll a
 
 type ScoreField = { category :: Category, score :: Maybe Int }
-type State = { scores :: Array ScoreField }
+type State = { dice :: Array Int, scores :: Array ScoreField }
 
 initialState :: State
-initialState = { scores: [ 
+initialState = { dice: [1,2,3,4,5],
+                 scores: [ 
                            { category: Aces, score: Nothing },
                            { category: Twos, score: Just 4 }
                          ]
@@ -34,24 +38,31 @@ ui = component render eval
 
   render :: State -> ComponentHTML Query
   render state =
-    H.table_ [
-      H.tbody_ (map renderScoreField state.scores)
+    H.div_ [
+      H.div_ [ H.text (show state.dice), H.button [E.onClick (E.input_ Roll)] [ H.text "Würfeln" ] ],
+      H.table_ [
+        H.tbody_ (map renderScoreField state.scores)
+      ]   
     ]
 
   renderScoreField sf = H.tr_ [
                           H.td_ [ H.text (showCategory sf.category) ],
                           H.td props [ H.text label ]
                         ]
-    where props = if isJust sf.score then [ E.onClick (E.input_ (ScoreQuery sf.category)) ] else [] 
+    where props = if isNothing sf.score then [ E.onClick (E.input_ (ScoreQuery sf.category)) ] else [] 
           label = show sf.score
           showCategory Aces = "Einser"
           showCategory Twos = "Zweier"
           showCategory _ = "not yet translated"
 
   eval :: Natural Query (ComponentDSL State Query (Aff (random::RANDOM | eff)))
+  eval (Roll next) = do
+    dice <- liftEff' (sequence (replicate 5 (randomInt 1 6)))
+    modify (\state -> state { dice = dice })
+    pure next
+    
   eval (ScoreQuery category next) = do
---    die <- liftEff' (sequence [randomInt 1 6, randomInt 1 6, randomInt 1 6])
-    modify (\state -> { scores: map setScore state.scores })
+    modify (\state -> state { scores = map setScore state.scores })
     pure next
       where setScore sf = if sf.category == category then { category: category, score: Just 123 } else sf
 
