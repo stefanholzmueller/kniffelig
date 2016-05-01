855fbfa84f2099fbae71e58f3eb96ebe49a28854
Stefan Holzmüller <stefanholzmueller@github>
2016-05-01 08:20:02 +0200

    cleaned up rendering of score table

--- src/Main.purs
+++ src/Main.purs
@@ -7,7 +7,7 @@ import Control.Monad.Aff.Free (fromEff)
 import Control.Monad.Eff (Eff())
 import Control.Monad.Eff.Random
 import Data.Array (alterAt, filter, length, range, replicate, zip)
-import Data.Foldable (any)
+import Data.Foldable (any, find)
 import Data.Maybe
 import Data.Traversable (sequence)
 import Data.Tuple
@@ -47,32 +47,44 @@ ui = component { render, eval }
                     else "Alle Würfe sind aufgebraucht - eine Kategorie werten oder streichen!"
       ],
       H.table_ [
-        H.tbody_ $ map renderScoreRow upperSectionScores
-                ++ [ H.tr_ [
-                       H.td_ [ H.text "Zwischensumme" ],
-                       H.td_ [ H.text $ show $ state.game.sumUpperSection ]
-                   ] ]
-                ++ [ H.tr_ [
-                       H.td_ [ H.text "Bonus" ],
-                       H.td_ [ H.text $ show $ state.game.bonusUpperSection ]
-                   ] ]
-                ++ [ H.tr_ [
-                       H.td_ [ H.text "Zwischensumme oberer Teil" ],
-                       H.td_ [ H.text $ show $ state.game.finalUpperSection ]
-                   ] ]
-                ++ map renderScoreRow lowerSectionScores
-                ++ [ H.tr_ [
-                       H.td_ [ H.text "Zwischensumme unterer Teil" ],
-                       H.td_ [ H.text $ show $ state.game.sumLowerSection ]
-                   ] ]
-                ++ [ H.tr_ [
-                       H.td_ [ H.text "Endsumme" ],
-                       H.td_ [ H.text $ show $ state.game.finalSum ]
-                   ] ]
+        H.tbody_ $ [ scoreRow "Einser" Y.Aces
+                   , scoreRow "Zweier" Y.Twos
+                   , scoreRow "Dreier" Y.Threes
+                   , scoreRow "Vierer" Y.Fours
+                   , scoreRow "Fünfer" Y.Fives
+                   , scoreRow "Sechser" Y.Sixes
+                   , calculatedRow "Zwischensumme" state.game.sumUpperSection
+                   , calculatedRow "Bonus" state.game.bonusUpperSection
+                   , calculatedRow "Zwischensumme oberer Teil" state.game.finalUpperSection
+                   , scoreRow "Dreierpasch" Y.ThreeOfAKind
+                   , scoreRow "Viererpasch" Y.FourOfAKind
+                   , scoreRow "Full House" Y.FullHouse
+                   , scoreRow "Kleine Straße" Y.SmallStraight
+                   , scoreRow "Große Straße" Y.LargeStraight
+                   , scoreRow "Yahtzee!" Y.Yahtzee
+                   , scoreRow "Chance" Y.Chance
+                   , calculatedRow "Zwischensumme unterer Teil" state.game.sumLowerSection
+                   , calculatedRow "Endsumme" state.game.finalSum
+                   ]
       ],
       H.p_ if state.game.gameOver then [ H.button [ E.onClick (E.input_ Restart) ] [ H.text "Neues Spiel" ] ] else []
     ]
     where
+    calculatedRow label score = H.tr_ [
+                                  H.td_ [ H.text label ],
+                                  H.td_ [ H.text $ show score ]
+                                ]
+    scoreRow label category = H.tr_ [
+                                H.td_ [ H.text label ],
+         case scoreState of
+           Y.Scored maybe ->    H.td [ P.classes [ C.className "scored" ] ] [ H.text $ showJust maybe ]
+           Y.Option (Just o) -> H.td [ onclick, P.classes [ C.className "option" ] ] [ H.text $ show o ]
+           Y.Option Nothing ->  H.td [ onclick, P.classes [ C.className "discard" ] ] [ H.text "-" ]
+                              ]
+      where onclick = E.onClick (E.input_ (Score category))
+            showJust maybe = fromMaybe "-" $ map show maybe
+            scoreState = fromMaybe (Y.Option Nothing) $ map (_.state) $ maybeFind
+            maybeFind = find (\sf -> sf.category == category) state.game.scoreColumn.scores
     rerollsAllowed = state.rerolls < Y.maxRerolls
     rerollsPossible = Y.maxRerolls - state.rerolls
     anyDieMarked = any (\d -> d.marked) state.dice
@@ -83,34 +95,6 @@ ui = component { render, eval }
       where
       classes = P.classes ([ C.className "die" ] ++ if die.marked then [ C.className "marked" ] else [])
       onclick = E.onClick (E.input_ (MarkDie i))
-
-    columnGetters game = [] 
-                      ++ [ game.sumUpperSection, game.bonusUpperSection, game.finalUpperSection ]
-                      ++ []
-                      ++ [ game.sumLowerSection. game.finalSum ]
-    renderScoreRow sf = H.tr_ [
-                          H.td_ [ H.text (showCategory sf.category) ],
-                          case sf.state of
-                               Y.Scored maybe -> H.td [ P.classes [ C.className "scored" ] ] [ H.text $ showJust maybe ]
-                               Y.Option (Just o) -> H.td [ onclick, P.classes [ C.className "option" ] ] [ H.text $ show o ]
-
-                               Y.Option Nothing -> H.td [ onclick, P.classes [ C.className "discard" ] ] [ H.text "-" ]
-                        ]
-      where onclick = E.onClick (E.input_ (Score sf.category))
-            showJust maybe = fromMaybe "-" $ map show maybe
-            showCategory Y.Aces = "Einser"
-            showCategory Y.Twos = "Zweier"
-            showCategory Y.Threes = "Dreier"
-            showCategory Y.Fours = "Vierer"
-            showCategory Y.Fives = "Fünfer"
-            showCategory Y.Sixes = "Sechser"
-            showCategory Y.ThreeOfAKind = "Dreierpasch"
-            showCategory Y.FourOfAKind = "Viererpasch"
-            showCategory Y.FullHouse = "Full House"
-            showCategory Y.SmallStraight = "Kleine Straße"
-            showCategory Y.LargeStraight = "Große Straße"
-            showCategory Y.Yahtzee = "Yahtzee!"
-            showCategory Y.Chance = "Chance"
 
   eval :: Natural Query (ComponentDSL State Query (Aff (AppEffects eff)))
   eval (Reroll next) = do
